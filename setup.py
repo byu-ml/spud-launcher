df84f593ea8ac4d95c4fe244240b35a49a19ad46
Lawrence Thatcher <lwthatcher@msn.com>
2017-04-10 14:33:59 -0600

    initializing repo

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup for GFMM"""
+
+from distutils.core import setup
+
+setup(
+    name='spud-launcher',
+    version='0.1.0',
+    description='Distribute python jobs over the potatoes',
+    author='Lawrence Thatcher',
+    author_email='lwthatcher@msn.com',
+    url='https://github.com/byu-ml/spud-launcher',
+    packages=['spuds']
+)
