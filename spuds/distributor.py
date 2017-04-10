c33b56384e56e2dfca7d5c517d4f5324db0c1464
Lawrence Thatcher <lwthatcher@msn.com>
2017-04-10 17:31:41 -0600

    inital free-potato

--- spuds/distributor.py
+++ spuds/distributor.py
@@ -1,5 +1,6 @@
 import asyncio
 import asyncssh
+import re
 from spuds import Potatoes
 
 
@@ -12,8 +13,9 @@ async def run_multiple_clients():
     potatoes = Potatoes()
     hosts = potatoes.all
 
-    tasks = (run_client(host, 'hostname') for host in hosts)
+    tasks = [run_client(host, 'hostname && uptime') for host in hosts]
     results = await asyncio.gather(*tasks, return_exceptions=True)
+    # results, pending = await asyncio.wait(tasks)
 
     for i, result in enumerate(results, 1):
         if isinstance(result, Exception):
@@ -23,8 +25,17 @@ async def run_multiple_clients():
             print(result.stderr, end='')
         else:
             print('Task %d succeeded:' % i)
-            print(result.stdout, end='')
-
+            print(parse_load(result.stdout), end='')
+        print()
         print(75*'-')
+
+
+def parse_load(line):
+    """Parses the load from a line of uptime output"""
+    if 'load average' in line:
+        samples = [float(val.replace(',', '')) for val in line.split()[-3:]]
+        return sum(samples) / len(samples)
+    else:
+        return 0
 
 asyncio.get_event_loop().run_until_complete(run_multiple_clients())
---
+++ spuds/free_potato.py
@@ -0,0 +1,53 @@
+import asyncio
+import asyncssh
+import argparse
+from spuds import Potatoes
+
+
+async def run_client(host, command):
+    async with asyncssh.connect(host, known_hosts=None) as conn:
+        return await conn.run(command)
+
+async def free_potatoes(hosts, limit=4.):
+    tasks = [run_client(host, 'uptime') for host in hosts]
+    responses = await asyncio.gather(*tasks, return_exceptions=True)
+
+    results = []
+
+    for i, result in enumerate(responses):
+        available = False
+        r = {'host': hosts[i], 'message': None, 'load': 0}
+        if isinstance(result, Exception):
+            r['message'] = 'Task %d failed: %s' % (i, str(result))
+        elif result.exit_status != 0:
+            r['message'] = 'Task %d exited with status %s: %s' % (i, result.exit_status, result.stderr)
+        else:
+            r['message'] = 'Task %d succeeded:' % i
+            value = parse_load(result)
+            if value <= limit:
+                available = True
+                r['load'] = value
+        if available:
+            results.append(r)
+    return sorted(results, key=lambda _r: _r['load'])
+
+
+def parse_load(result):
+    """Parses the load from a line of uptime output"""
+    if hasattr(result, 'stdout') and 'load average' in result.stdout:
+        samples = [float(val.replace(',', '')) for val in result.stdout.split()[-3:]]
+        return sum(samples) / len(samples)
+    else:
+        return 0
+
+
+parser = argparse.ArgumentParser(description='returns a sorted list with the most available potatoes')
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    # for now hard code to all potatoes
+    p = Potatoes()
+    hosts = p.all
+    r = asyncio.get_event_loop().run_until_complete(free_potatoes(hosts))
+    print(r)
