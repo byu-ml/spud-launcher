f458e4a4facbb6d05f7b3b3b219df5d69bb24aa3
Lawrence Thatcher <lwthatcher@msn.com>
2017-04-12 20:15:04 -0600

    seems to do event looping

--- spuds/distributor.py
+++ spuds/distributor.py
@@ -16,22 +16,45 @@ async def run_multiple_clients(hosts, commands):
     # tasks = [run_client(host, 'pwd') for host in hosts]
     # results = await asyncio.gather(*tasks, return_exceptions=True)
     available = await free_potatoes(hosts)
-    # print(available)
+    for comms in get_commands(commands, available):
+        tasks = [run_client(h[0], 'echo "echo: ' + c + '"') for c, h in zip(comms, available)]
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+        for i, result in enumerate(results):
+            if isinstance(result, Exception):
+                print('Task %d failed: %s' % (i, str(result)))
+            elif result.exit_status != 0:
+                print('Task %d exited with status %s:' % (i, result.exit_status))
+                print(result.stderr, end='')
+            else:
+                print('Task %d succeeded:' % i)
+                print(result.stdout)
+            print()
+            print(75*'-')
+        # update availability
+        _next = await free_potatoes(hosts)
+        available = await asyncio.sleep(2, _next)
 
-    tasks = [run_client(host[0], 'pwd') for host in available]
-    results = await asyncio.gather(*tasks, return_exceptions=True)
+    # tasks = [run_client(host[0], 'pwd') for host in available]
+    # results = await asyncio.gather(*tasks, return_exceptions=True)
+    #
+    # for i, result in enumerate(results):
+    #     if isinstance(result, Exception):
+    #         print('Task %d failed: %s' % (i, str(result)))
+    #     elif result.exit_status != 0:
+    #         print('Task %d exited with status %s:' % (i, result.exit_status))
+    #         print(result.stderr, end='')
+    #     else:
+    #         print('Task %d succeeded:' % i)
+    #         print(result.stdout)
+    #     print()
+    #     print(75*'-')
 
-    for i, result in enumerate(results):
-        if isinstance(result, Exception):
-            print('Task %d failed: %s' % (i, str(result)))
-        elif result.exit_status != 0:
-            print('Task %d exited with status %s:' % (i, result.exit_status))
-            print(result.stderr, end='')
-        else:
-            print('Task %d succeeded:' % i)
-            print(result.stdout)
-        print()
-        print(75*'-')
+
+def get_commands(_commands, available):
+    _i = 0
+    while _i < len(_commands):
+        yield _commands[_i:_i + len(available)]
+        _i += len(available)
 
 
 def parse_params(params):
